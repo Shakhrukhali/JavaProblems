bced007d9f724a676a0d03540ca25813ae99c4a2
Shakhrukhali <shakhrukhali@gmail.com>
2023-05-31 16:43:05 +0500

    Musbat bo'lsa birga oshir

---
+++ MusbatBolsaBirgaOshir.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MusbatBolsaBirgaOshir {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("number : ");
+        int number = scanner.nextInt();
+        if (number > 0) {
+            number++;
+        } else {
+            number = number;
+        }
+        System.out.println(number);
+    }
+}
