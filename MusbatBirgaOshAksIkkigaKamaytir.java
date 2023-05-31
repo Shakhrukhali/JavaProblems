93ffe94910df4a481552268f01cce3536247728f
Shakhrukhali <shakhrukhali@gmail.com>
2023-05-31 16:45:40 +0500

    Musbat bo'lsa birga oshir Aks Holda ikkiga kamaytir

---
+++ MusbatBirgaOshAksIkkigaKamaytir.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MusbatBirgaOshAksIkkigaKamaytir {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("number : ");
+        int number = scanner.nextInt();
+        if (number > 0) {
+            number++;
+        } else {
+            number -= 2;
+        }
+        System.out.println(number);
+    }
+}
+
