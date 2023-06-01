ff84ea01f1e954c8b0e2fe89c7841d8f91f8554e
Shakhrukhali <shakhrukhali@gmail.com>
2023-06-01 18:30:09 +0500

    1+a+a^2+a^3+...a^n hisoblovchi dastur

---
+++ Aning1DanNgachaBulganDarajariSum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Aning1DanNgachaBulganDarajariSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = 2.5d;
+        double sum = 0;
+        System.out.print("n = ");
+        int n = scanner.nextInt();
+        for (int i = 0; i <= n; i++) {
+            System.out.print(Math.pow(a,i)+ " + ");
+            sum += Math.pow(a, i);
+        }
+
+
+        System.out.println(" = " + sum);
+
+    }
+}
