b7cc18d1c479526271e6c2d846a72c8576fc8b60
Shakhrukhali <shakhrukhali@gmail.com>
2023-06-28 23:41:13 +0500

    toLowercase va toUpperCase Metodini yozish

--- ImmutableString/UpperCaseMethod.java
+++ ImmutableString/UpperCaseMethod.java
@@ -2,25 +2,25 @@ package ImmutableString;
 
 import java.util.Scanner;
 
-public class LeetCode709LowerCase {
+public class UpperCaseMethod {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("So'z kiriting : ");
         String s = scanner.nextLine();
-        System.out.println(lowerCase(s));
+
+        System.out.println(upperCase(s));
 
     }
 
-    static String lowerCase(String s) {
+    private static String upperCase(String s) {
         StringBuilder st = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c >= 'A' && c <= 'Z') {
-                c = (char) (c + 32);
+            if (c >= 'a' && c <= 'z') {
+                c = (char) (c - 32);
             }
             st.append(c);
         }
-
         return st.toString();
     }
 }
